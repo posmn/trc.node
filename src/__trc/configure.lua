4f76c5261758c315f15a360db3e298a2593365d5
cryi <cryi@tutanota.com>
2023-08-23 13:53:42 +0200

    fixes

--- src/__trc/configure.lua
+++ src/__trc/configure.lua
@@ -1,25 +1,31 @@
 local SENTINEL_URL = "https://github.com/terracoin/sentinel/archive/refs/heads/master.zip"
+local SENTINEL_DIRECTORY = "bin/sentinel"
 
 log_info("Installing " .. am.app.get("id") .. " sentinel...")
 
-local _ok, _code, _, _error, _dep = _apt.install("python python-virtualenv")
-if not _ok then 
+local _ok, _apt = am.plugin.safe_get("apt")
+if not _ok then
+        log_warn("Failed to load apt plugin!")
+end
+local _ok, _code, _, _error, _dep = _apt.install("python2.7 virtualenv python2-pip-whl python2-setuptools-whl")
+if not _ok then
     log_warn("Failed to install " .. (_dep or '-').. "! - " .. _error)
 end
 
 local _tmpFile = os.tmpname()
-local _ok, _error = net.safe_download_file(SENTINEL_URL, _tmpFile)
+local _ok, _error = net.safe_download_file(SENTINEL_URL, _tmpFile, {followRedirects = true})
 if not _ok then
     os.remove(_tmpFile)
     ami_error("Failed to download sentinel - " .. _error .. "!")
 end
 
-local _ok, _error = zip.safe_extract(_tmpFile, "bin/sentinel", { flattenRootDir = true })
+fs.mkdirp(SENTINEL_DIRECTORY)
+local _ok, _error = zip.safe_extract(_tmpFile, SENTINEL_DIRECTORY, { flattenRootDir = true })
 os.remove(_tmpFile)
 ami_assert(_ok, "Failed to extract sentinel - " .. (_error or "") .. "!")
 
-os.chdir("bin/sentinel")
-local cmd = "virtualenv ./venv && ./venv/bin/pip install -r requirements.txt"
+os.chdir(SENTINEL_DIRECTORY)
+local cmd = "virtualenv -p /usr/bin/python2.7 ./venv && ./venv/bin/pip install -r requirements.txt"
 local ok = os.execute(cmd)
 
 if not ok then
